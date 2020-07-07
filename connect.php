a2a38dd9003646f97fa4d6dfb1076fc8221c990d
oTungTran <66772475+oTungTran@users.noreply.github.com>
2020-07-07 09:04:51 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
-$conn_string="host=ec2-34-197-141-7.compute-1.amazonaws.com dbname=d8utqot0l3p18p user=tteblxrbhxziyj password=6cd2486ab041cf5da0900760f8e916a9f2a8a8510c247deb820bfeb0912b6d01";
+$conn_string="host=ec2-54-81-37-115.compute-1.amazonaws.com port=5432
+ dbname=dbi738tg8htnh2 user=nxuguqscxtzzbs password=a30cb700104ae1a63d23d88d7ea43dc577c5cd48356117eb5d52143095284a14";
 $dbconn= pg_connect($conn_string);
 if (isset($_POST['username'])) {
 	# code...
